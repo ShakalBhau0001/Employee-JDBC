2583122213cf80eac8686843153a468f7b013531
Shakal Bhau <160219957+ShakalBhau0001@users.noreply.github.com>
2025-09-10 17:33:20 +0530

    InsertingRecord.java

---
+++ InsertingRecord.java
@@ -0,0 +1,50 @@
+package jdbcTask;
+
+import java.sql.*;
+import java.util.Scanner;
+import oracle.jdbc.OracleDriver;
+
+public class InsertingRecord {
+	public static void main(String[] args) throws Exception{
+		DriverManager.registerDriver(new OracleDriver());
+		Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE","bca3","college123");
+		Scanner sc = new Scanner(System.in);
+		
+		while(true){
+			System.out.println("Enter Employee Id:-");
+			int id= sc.nextInt();
+			sc.nextLine();
+			System.out.println("Enter Employee Name:-");
+			String name= sc.nextLine();
+			System.out.println("Enter Employee Address:-");
+			String address = sc.nextLine();
+			System.out.println("Enter Employee Mobile No:");
+			long mobile_no= sc.nextLong();
+			sc.nextLine();
+			System.out.println("Enter Employee Designation:");
+			String designation= sc.nextLine();
+			System.out.println("Enter Employee Department:");
+			String department= sc.nextLine();
+			System.out.println("Enter Employee Salary:");
+			float salary = sc.nextFloat();
+			sc.nextLine();
+			
+			PreparedStatement ps = con.prepareStatement("insert into BCA3 values(?,?,?,?,?,?,?)");
+			ps.setInt(1, id);
+			ps.setString(2, name);
+			ps.setString(3, address);
+			ps.setLong(4, mobile_no);
+			ps.setString(5, designation);
+			ps.setString(6, department);
+			ps.setFloat(7, salary);
+			int count = ps.executeUpdate();
+			System.out.println(count+" "+"row created");
+			
+        	System.out.println("Do You Want To Record Insert [Y/N]");			
+			String ch = sc.nextLine();
+			if(ch.equalsIgnoreCase("N"))
+				break;
+		}
+		con.close();
+	}
+}
